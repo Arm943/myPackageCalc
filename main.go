e3ca9dc76230191d086036e284d6e1327cb837fa
ArmenTs <stalin943@gmail.com>
2025-04-10 13:08:44 +0300

    v2.1.0 добавлен функционал работы с содержимым файлов

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 var name string
 var newName string
 
+// ФУНКЦИИ ДЛЯ РАБОТЫ С ФАЙЛАМИ
 // создание файла
 func createFile(name string) {
 	file, err := os.Create(name)
@@ -15,7 +19,7 @@ func createFile(name string) {
 		fmt.Println("Ошибка при создании файла:", err)
 	}
 	defer file.Close()
-	fmt.Println("Файл создан:", name)
+	fmt.Println("✅ Файл создан:", name)
 }
 
 // переименовать файл
@@ -24,7 +28,7 @@ func renameFile(name, newName string) {
 	if err != nil {
 		fmt.Println("Ошибка при переименовывании файла:", err)
 	}
-	fmt.Println("Новое имя файла:", newName)
+	fmt.Println("✅ Новое имя файла:", newName)
 }
 
 // проверка существует ли файл
@@ -33,7 +37,7 @@ func existFile(name string) {
 	if os.IsNotExist(err) {
 		fmt.Println("Файл не существует")
 	} else {
-		fmt.Println("Файл существует")
+		fmt.Println("✅ Файл существует")
 	}
 }
 
@@ -43,17 +47,88 @@ func deleteFile(name string) {
 	if err != nil {
 		fmt.Println("Ошибка при удалении файла:", err)
 	}
-	fmt.Printf("Файл %s удален \n", name)
+	fmt.Printf("✅ Файл %s удален \n", name)
+}
+
+// ФУНКЦИИ ДЛЯ РАБОТЫ С СОДЕРЖИМЫМ ФАЙЛОВ
+
+// запись текста в файл
+func writeNewText(fileName string, userText string) {
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Println("Ошибка отрытия файла:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write([]byte(userText))
+	if err != nil {
+		fmt.Println("Ошибка записи:", err)
+	}
+	fmt.Println("✅ ваш текст успешно сохранен")
+
+}
+
+// чтение текста в файле
+func readFile(fileName string) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла:", err)
+		return
+	}
+	defer file.Close()
+
+	buf := make([]byte, 1024)
+	n, err := file.Read(buf)
+	if err != nil && err != io.EOF {
+		fmt.Println("Ошибка чтения:", err)
+		return
+	}
+	fmt.Println("Содержимое файла:")
+	fmt.Println(string(buf[:n]))
+}
+
+// копирование текста из одно файла в другой
+func copyText(fileOne, fileTwo string) {
+	fOne, err := os.Open(fileOne)
+	if err != nil {
+		fmt.Println("ошибка при отрытии первого файла: ", err)
+		return
+	}
+	defer fOne.Close()
+
+	fTwo, err := os.OpenFile(fileTwo, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Println("ошибка при отрытии второго файла: ", err)
+		return
+	}
+	defer fTwo.Close()
+
+	_, err = io.Copy(fTwo, fOne)
+	if err != nil {
+		fmt.Println("Ошибка при копировании:", err)
+		return
+	}
+
+	fmt.Printf("Все данные из файла %v успешно скопированы в файл %v ", fileOne, fileTwo)
 }
 
 func menu() {
 	for {
 		var userInput string
-		fmt.Println(`Выберите действие:
-	1 - создать файл
-	2 - переименовать файл
-	3 - проверить файл
-	4 - удалить файл
+		fmt.Println()
+		fmt.Println(`--------------------------------
+👉 Выберите действие:
+   1️⃣ - создать файл
+   2️⃣ - переименовать файл
+   3️⃣ - проверить файл
+   4️⃣ - удалить файл
+	5️⃣ - запись текста в файл
+	6️⃣ - показать текст файле
+	7️⃣ - скопировать текст в другой файл
+	8️⃣ - todo
+--------------------------------
+🅾️ - выход
 	`)
 		fmt.Scan(&userInput)
 		switch userInput {
@@ -80,7 +155,38 @@ func menu() {
 			fmt.Print("Введите название файла: ")
 			fmt.Scan(&name)
 			deleteFile(name)
+		case "5":
+			var fileName string
+			fmt.Print("Введите название файла: ")
+			fmt.Scan(&fileName)
+			fmt.Scanln()
+
+			fmt.Print("Введите свой текст: ")
+			reader := bufio.NewReader(os.Stdin)
+			userText, _ := reader.ReadString('\n')
+			userText = strings.TrimSpace(userText)
+			writeNewText(fileName, userText)
+		case "6":
+			var fileName string
+			fmt.Print("Введите название файла: ")
+			fmt.Scan(&fileName)
+			fmt.Println()
+			readFile(fileName)
+		case "7":
+			var fileOne string
+			var fileTwo string
+
+			fmt.Println("Введите название файла ОТКУДА хотите скопировать текст: ")
+			fmt.Scan(&fileOne)
+			fmt.Println("Введите название файла КУДА хотите скопировать текст: ")
+			fmt.Scan(&fileTwo)
+			fmt.Println()
+			copyText(fileOne, fileTwo)
+
+		case "0":
+			os.Exit(0)
 		}
+
 	}
 }
 
